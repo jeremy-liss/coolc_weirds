537ca58115d44ad9958126abfb37c3fa0c0008fb
daniel-reason <dreason@live.co.uk>
2017-03-17 14:31:18 +1300

    refactored html into cool.js

--- components/cool.js
+++ components/cool.js
@@ -4,6 +4,33 @@ module.exports = Cool
 
 function Cool (){
   return (
-    <h1>Hey coolio!</h1>
+    <div>
+      <h1>Hey coolio!</h1>
+      <p>
+        <button onClick={myMove}>
+          Click Me
+        </button>
+      </p>
+      <div id="container">
+        <div id="animate"></div>
+      </div>
+    </div>
   )
+}
+
+
+function myMove() {
+  var elem = document.getElementById("animate")
+  var pos = 0
+  var id = setInterval(frame, 1)
+
+  function frame() {
+    if (pos == 570) {
+      clearInterval(id)
+    } else {
+      pos++;
+      elem.style.top = pos + 'px'
+      elem.style.left = pos + 'px'
+    }
+  }
 }
